2a6bc11b11b5980b9d70800307a81cad9513904d
Ravil S <ravil.nell@gmail.com>
2026-01-17 20:30:09 +0100

    set up global validation pipe, update DTOs

--- src/modules/tasks/application/dto/assign-person.dto.ts
+++ src/modules/tasks/application/dto/assign-person.dto.ts
@@ -1,3 +1,8 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class AssignPersonDto {
+  @IsString()
+  @IsNotEmpty()
+  // @IsUUID() - TODO: use uuid in production, commented out to test on local
   personId: string;
 }
--- src/modules/tasks/application/dto/create-task.dto.ts
+++ src/modules/tasks/application/dto/create-task.dto.ts
@@ -1,9 +1,28 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEnum,
+  IsOptional,
+  IsDateString,
+} from 'class-validator';
 import { Priority, TaskStatus } from '../../domain/task.entity.js';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsEnum(TaskStatus)
   status: TaskStatus;
+
+  @IsEnum(Priority)
   priority: Priority;
+
+  @IsOptional()
+  @IsDateString()
   dueDate?: Date;
 }
