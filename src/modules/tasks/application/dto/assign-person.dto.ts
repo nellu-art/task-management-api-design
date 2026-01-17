85239d85433c5842769d2f28136572006d56cdaf
Ravil S <ravil.nell@gmail.com>
2026-01-17 20:25:22 +0100

    update tasks controller, add other route handlers

---
+++ src/modules/tasks/application/dto/assign-person.dto.ts
@@ -0,0 +1,3 @@
+export class AssignPersonDto {
+  personId: string;
+}
--- src/modules/tasks/presentation/tasks.controller.ts
+++ src/modules/tasks/presentation/tasks.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TasksService } from '../application/tasks.service.js';
 import { Task } from '../domain/task.entity.js';
 import { CreateTaskDto } from '../application/dto/create-task.dto.js';
+import { UpdateTaskDto } from '../application/dto/update-task.dto.js';
+import { AssignPersonDto } from '../application/dto/assign-person.dto.js';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,8 +22,45 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get(':id')
+  async getTaskById(@Param('id') id: string): Promise<Task | null> {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Put(':id')
+  async updateTask(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    return this.tasksService.updateTask(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') id: string): Promise<boolean> {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Post(':id/assign')
+  async assignPersonToTask(
+    @Param('id') id: string,
+    @Body() assignPersonDto: AssignPersonDto,
+  ): Promise<Task> {
+    return this.tasksService.assignPersonToTask(id, assignPersonDto.personId);
+  }
+
+  @Post(':id/unassign')
+  async unassignPerson(
+    @Param('id') id: string,
+    @Body() assignPersonDto: AssignPersonDto,
+  ): Promise<Task> {
+    return this.tasksService.unassignPersonFromTask(
+      id,
+      assignPersonDto.personId,
+    );
   }
 }
